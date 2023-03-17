a57d773c8f037a8df20288b0c1522785b4ef9678
harcenkom954 <104391181+harcenkom954@users.noreply.github.com>
2023-03-17 19:45:19 +0300

    Update parse_ref.py

--- parse_ref.py
+++ parse_ref.py
@@ -6,10 +6,8 @@ import re
 
 def set_output(name: str, val: str) -> None:
     if os.getenv("GITHUB_OUTPUT"):
-        with open(str(os.getenv("GITHUB_OUTPUT")), "a") as env:
             print(f"{name}={val}", file=env)
     else:
-        print(f"::set-output name={name}::{val}")
 
 
 def main() -> None:
@@ -21,7 +19,6 @@ def main() -> None:
             set_output("branch", stripped)
         elif category == "pull":
             set_output("branch", "pull/" + stripped.split("/")[0])
-        elif category == "tags":
             set_output("tag", stripped)
 
 
